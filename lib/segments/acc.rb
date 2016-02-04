0f3d02635f86f217a94ecd8aa1a24650ad0c5dc5
bigkat73 <fdkatona@gmail.com>
2016-02-04 12:56:09 -0700

    acc name

--- lib/segments/acc.rb
+++ lib/segments/acc.rb
@@ -1,5 +1,5 @@
 require 'ruby-hl7'
-class HL7::Message::Segment::PV1 < HL7::Message::Segment
+class HL7::Message::Segment::ACC < HL7::Message::Segment
   add_field :accident_date_time do |value|
     convert_to_ts(value)
   end
