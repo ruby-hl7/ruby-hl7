96d5d149df91d89bc92ccd92786a7ca3fc7e0f66
Casey Provost <provost.design@gmail.com>
2016-09-17 19:41:56 -0400

    convert date and time objects
    Have to convert date and time objects to hl7 safe strings.

--- lib/segments/dg1.rb
+++ lib/segments/dg1.rb
@@ -4,7 +4,9 @@ module HL7
     add_field :set_id,                    :idx => 1
     add_field :diagnosis_coding_method,   :idx => 2
     add_field :diagnosis_code,            :idx => 3
-    add_field :diagnosis_date_time,       :idx => 4
+    add_field :diagnosis_date_time,       :idx => 4 do |value|
+      convert_to_ts(value)
+    end
     add_field :diagnosis_type,            :idx => 5
     add_field :major_diagnostic_category, :idx => 6
     add_field :diagnosis_related_group,   :idx => 7
@@ -18,6 +20,18 @@ module HL7
     add_field :diagnosis_clinician,       :idx => 15
     add_field :diagnosis_classification,  :idx => 16
     add_field :confidential_indicator,    :idx => 17
-    add_field :attestation_date_time,     :idx => 18
+    add_field :attestation_date_time,     :idx => 18 do |value|
+      convert_to_ts(value)
+    end
+    
+    private
+    
+    def self.convert_to_ts(value) #:nodoc:
+      if value.is_a?(Time) or value.is_a?(Date)
+        value.to_hl7
+      else
+        value
+      end
+    end
   end
 end
