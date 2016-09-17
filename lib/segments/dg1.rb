4e137c2ba35153adb55572d3704b0d72d4295e3f
Casey Provost <provost.design@gmail.com>
2016-09-17 19:45:06 -0400

    remove index definition
    Don't have to have index defined if they are sequential

--- lib/segments/dg1.rb
+++ lib/segments/dg1.rb
@@ -1,26 +1,26 @@
 module HL7
   class Message::Segment::DG1 < HL7::Message::Segment
     weight 92
-    add_field :set_id,                    :idx => 1
-    add_field :diagnosis_coding_method,   :idx => 2
-    add_field :diagnosis_code,            :idx => 3
-    add_field :diagnosis_date_time,       :idx => 4 do |value|
+    add_field :set_id
+    add_field :diagnosis_coding_method
+    add_field :diagnosis_code
+    add_field :diagnosis_date_time do |value|
       convert_to_ts(value)
     end
-    add_field :diagnosis_type,            :idx => 5
-    add_field :major_diagnostic_category, :idx => 6
-    add_field :diagnosis_related_group,   :idx => 7
-    add_field :drg_approval_indicator,    :idx => 8
-    add_field :drg_grouper_review_code,   :idx => 9
-    add_field :outlier_type,              :idx => 10
-    add_field :outlier_days,              :idx => 11
-    add_field :outlier_cost,              :idx => 12
-    add_field :grouper_version_and_type,  :idx => 13
-    add_field :diagnosis_priority,        :idx => 14
-    add_field :diagnosis_clinician,       :idx => 15
-    add_field :diagnosis_classification,  :idx => 16
-    add_field :confidential_indicator,    :idx => 17
-    add_field :attestation_date_time,     :idx => 18 do |value|
+    add_field :diagnosis_type
+    add_field :major_diagnostic_category
+    add_field :diagnosis_related_group
+    add_field :drg_approval_indicator
+    add_field :drg_grouper_review_code
+    add_field :outlier_type
+    add_field :outlier_days
+    add_field :outlier_cost
+    add_field :grouper_version_and_type
+    add_field :diagnosis_priority
+    add_field :diagnosis_clinician
+    add_field :diagnosis_classification
+    add_field :confidential_indicator
+    add_field :attestation_date_time do |value|
       convert_to_ts(value)
     end
     
