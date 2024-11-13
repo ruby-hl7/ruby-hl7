b0147389147ee8399bcfe94523eaa8e5ea6f0cde
Lucas Montorio <lucas.montorio@lifen.fr>
2024-11-13 18:21:14 +0100

    chore: update existing specs accordingly

--- spec/helpers/string_formatter_spec.rb
+++ spec/helpers/string_formatter_spec.rb
@@ -4,18 +4,18 @@ require "spec_helper"
 
 RSpec.describe StringFormatterHelper, :type => :helper do
   describe "#hl7_batch?" do
-    context 'when string has a batch identifier' do
+    context "when string has a batch identifier" do
       let(:string) { HL7MESSAGES[:realm_batch] }
 
-      it 'returns true' do
+      it "returns true" do
         expect(hl7_batch?(string)).to be true
       end
     end
 
-    context 'when string does not have a batch identifier' do
+    context "when string does not have a batch identifier" do
       let(:string) { HL7MESSAGES[:realm_minimal_message] }
 
-      it 'returns false' do
+      it "returns false" do
         expect(hl7_batch?(string)).to be false
       end
     end
